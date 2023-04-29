7b1509c30adc4d611e8d54161398d5cf6de9c445
[mridul] <[smridul191006@bscse.uiu.ac.bd]>
2023-04-30 02:08:43 +0600

    hi

---
+++ src/Pages/Faq/Faq/Faq.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Faq = () => {
+    return (
+        <div className='h-screen'>
+            <div>
+      <h1>Frequently Asked Questions</h1>
+      <h2>What is FundFuture?</h2>
+      <p>FundFuture is a crowdfunding platform that connects entrepreneurs and startups with investors who are looking for new and innovative ideas to invest in.</p>
+      <h2>How does FundFuture work?</h2>
+      <p>Entrepreneurs and startups can create a campaign on FundFuture to showcase their project and attract potential investors. Investors can then browse the campaigns and decide which projects they want to</p>
+        </div>
+        </div>
+    );
+};
+
+export default Faq;
