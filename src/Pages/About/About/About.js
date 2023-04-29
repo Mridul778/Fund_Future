19ea143dc031e04133b201e6ef47607356c98c63
Awlad <awlad.hossain.1123@gmail.com>
2023-04-30 02:41:54 +0600

    commit

---
+++ src/Pages/About/About/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className='h-screen'>
+            <h1>This is Abo adasdasdut</h1>
+            <h1>This is Abo adasdasdut</h1>
+            <h1>This is Abo adasdasdut</h1>
+            <h1>This is Abo adasdasdut</h1>
+            <h1>This is Abo adasdasdut</h1>
+            <h1>This is Abo adasdasdut</h1>
+            <h1>This is Abo adasdasdut</h1>
+            <h1>This is Abo adasdasdut</h1>
+            <h1>This is Abo adasdasdut</h1>
+            <h1>This is Abo adasdasdut</h1>
+            <h1>This is Abo adasdasdut</h1>
+            <h1>This is Abo adasdasdut</h1>
+            <h1>This is Abo adasdasdut</h1>
+            <h1>This is Abo adasdasdut</h1>
+        </div>
+    );
+};
+
+export default About;
